9ddb2a069da9d1aeb062bd57b31aa88006efcbf7
VISHAL KUMAR <54323540+vishalkumar19@users.noreply.github.com>
2025-07-17 17:03:19 +0530

    Update backend.tf

--- eks/backend.tf
+++ eks/backend.tf
@@ -7,11 +7,9 @@ terraform {
     }
   }
   backend "s3" {
-    bucket         = "dev-aman-tf-bucket"
+    bucket         = "dev-vishal-tf-bucket"
     region         = "us-east-1"
     key            = "eks/terraform.tfstate"
-    dynamodb_table = "Lock-Files"
-    encrypt        = true
   }
 }
 
